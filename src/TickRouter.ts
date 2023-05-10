241c4df9f337ceb8cd76f46626bdae9530d8d749
Vanya A. Sergeev <v@sergeev.io>
2023-05-10 01:26:58 -0500

    implement rate weighted routing in TickRouter

--- src/TickRouter.ts
+++ src/TickRouter.ts
@@ -1,4 +1,4 @@
-import { EncodedTick, Index, DecodedTick, TickEncoder } from './Tick';
+import { EncodedTick, DecodedTick, TickEncoder } from './Tick';
 
 export interface LiquidityNode {
   tick: EncodedTick;
@@ -8,6 +8,12 @@ export interface LiquidityNode {
 interface DecodedLiquidityNode {
   tick: DecodedTick;
   available: bigint;
+}
+
+interface Route {
+  nodes: DecodedLiquidityNode[];
+  amount: bigint;
+  cost: bigint;
 }
 
 function minBigInt(a: bigint, b: bigint): bigint {
@@ -36,38 +42,85 @@ export class TickRouter {
   /* Internal Helpers */
   /****************************************************************************/
 
-  _mapDuration(duration: number): Index {
-    /* Find lowest matching duration */
-    for (let i = 0; i < this.durations.length; i++) {
-      if (duration <= this.durations[i]) return i;
-    }
-
-    throw new Error(`Unsupported duration ${duration} (max is ${this.durations[this.durations.length - 1]}).`);
+  _decodeNodes(nodes: LiquidityNode[]): DecodedLiquidityNode[] {
+    /* Decode tick in each node */
+    return nodes.map((n) => ({ tick: TickEncoder.decode(n.tick), available: n.available }));
   }
 
-  _filterNodes(nodes: LiquidityNode[], duration: number): DecodedLiquidityNode[] {
-    /* Decode liquidity nodes */
-    let decodedNodes = nodes.map((n) => ({ tick: TickEncoder.decode(n.tick), available: n.available }));
+  _filterNodes(nodes: DecodedLiquidityNode[], duration: number): DecodedLiquidityNode[] {
+    /* Map duration to duration index */
+    let durationIndex = this.durations.findIndex((d) => duration <= d);
+    durationIndex = durationIndex == -1 ? Infinity : durationIndex;
 
-    /* Map duration index */
-    const durationIndex = this._mapDuration(duration);
+    /* Filter nodes with durations exceeding input duration */
+    return nodes.filter((n) => n.tick.duration >= durationIndex);
+  }
 
-    /* Filter liquidity nodes by supported duration */
-    decodedNodes = decodedNodes.filter((n) => n.tick.duration >= durationIndex);
+  _aggregateNodes(nodes: DecodedLiquidityNode[]): DecodedLiquidityNode[][] {
+    const aggregatedNodes: DecodedLiquidityNode[][] = [];
 
-    /* Sort liquidity nodes by rate */
-    decodedNodes.sort((a, b) => (a.tick.limit <= b.tick.limit && a.tick.rate < b.tick.rate ? -1 : 1));
+    /* Aggregate all nodes with same limit */
+    let prevLimit = -1n;
+    for (const node of nodes) {
+      if (node.tick.limit == prevLimit) {
+        aggregatedNodes[aggregatedNodes.length - 1].push(node);
+      } else {
+        aggregatedNodes.push([node]);
+        prevLimit = node.tick.limit;
+      }
+    }
 
-    /* Filter liquidity nodes by lowest rate */
-    decodedNodes = decodedNodes.reduce(
-      (acc, n) => (acc.length == 0 || acc[acc.length - 1].tick.limit != n.tick.limit ? [...acc, n] : acc),
-      [] as DecodedLiquidityNode[],
+    return aggregatedNodes;
+  }
+
+  _traverseNodes(
+    aggregatedNodes: DecodedLiquidityNode[][],
+    multiplier: number,
+    nodes: DecodedLiquidityNode[] = [],
+    amount: bigint = 0n,
+    cost: bigint = 0n,
+  ): Route[] {
+    /* If we reached the end of the nodes */
+    if (aggregatedNodes.length == 0) {
+      return [];
+    }
+
+    let routes: Route[] = [];
+    for (const node of aggregatedNodes[0]) {
+      /* Source liquidity from this node and calculate its cost */
+      const nodeAmount = minBigInt(node.tick.limit * BigInt(multiplier) - amount, node.available);
+      const nodeCost = nodeAmount * BigInt(node.tick.rate + 1);
+
+      if (nodeAmount == 0n) {
+        /* If node amount is 0, skip the node */
+        routes = routes.concat(this._traverseNodes(aggregatedNodes.slice(1), multiplier, nodes, amount, cost));
+      } else {
+        /* Add the route that ends here and all routes that continue downstream */
+        routes = routes.concat(
+          { nodes: [...nodes, node], amount: amount + nodeAmount, cost: cost + nodeCost },
+          this._traverseNodes(
+            aggregatedNodes.slice(1),
+            multiplier,
+            [...nodes, node],
+            amount + nodeAmount,
+            cost + nodeCost,
+          ),
+        );
+      }
+    }
+
+    return routes;
+  }
+
+  _findBestRoute(routes: Route[]): Route {
+    /* Find route with greatest amount, followed by lowest cost, followed by lowest amount of nodes */
+    return routes.reduce(
+      (bestRoute, route) =>
+        route.amount > bestRoute.amount || (route.amount == bestRoute.amount && route.cost < bestRoute.cost)
+          ? route
+          : bestRoute,
+      routes.length == 0 ? { nodes: [], amount: 0n, cost: 0n } : routes[0],
     );
-
-    /* FIXME this will ultimately filter by lowest effective rate, taking
-     * amount available into account */
-
-    return decodedNodes;
   }
 
   _sourceNodes(nodes: DecodedLiquidityNode[], amount: bigint, multiplier: number): bigint {
@@ -94,11 +147,20 @@ export class TickRouter {
    * @returns Liquidity available
    */
   forecast(nodes: LiquidityNode[], duration: number, multiplier: number = 1, numNodes: number = 10): bigint {
-    const filteredNodes = this._filterNodes(nodes, duration).splice(-numNodes);
+    /* Discover all routes */
+    const routes = this._traverseNodes(
+      this._aggregateNodes(this._filterNodes(this._decodeNodes(nodes), duration)),
+      multiplier,
+    );
 
-    const amount = this._sourceNodes(filteredNodes, 2n ** 128n - 1n, multiplier);
+    /* Find best route */
+    const route = this._findBestRoute(routes);
 
-    return amount;
+    /* Take top number of nodes on route */
+    const topNodes = route.nodes.splice(-numNodes);
+
+    /* Source maximum liquidity from nodes */
+    return this._sourceNodes(topNodes, 2n ** 120n - 1n, multiplier);
   }
 
   /**
@@ -117,13 +179,23 @@ export class TickRouter {
     multiplier: number = 1,
     numNodes: number = 10,
   ): EncodedTick[] {
-    const filteredNodes = this._filterNodes(nodes, duration).splice(-numNodes);
+    /* Discover all routes */
+    const routes = this._traverseNodes(
+      this._aggregateNodes(this._filterNodes(this._decodeNodes(nodes), duration)),
+      multiplier,
+    );
 
-    const available = this._sourceNodes(filteredNodes, amount, multiplier);
-    if (available != amount) {
+    /* Find best route */
+    const route = this._findBestRoute(routes);
+
+    /* Take top number of nodes on route */
+    const topNodes = route.nodes.splice(-numNodes);
+
+    /* Check sufficinet liquidity is available in nodes */
+    if (this._sourceNodes(topNodes, amount, multiplier) != amount) {
       throw new Error(`Insufficient liquidity for ${amount} amount.`);
     }
 
-    return filteredNodes.map((n) => TickEncoder.encode(n.tick));
+    return topNodes.map((n) => TickEncoder.encode(n.tick));
   }
 }
--- test/TickRouter.spec.ts
+++ test/TickRouter.spec.ts
@@ -2,16 +2,13 @@ import { TickRouter, LiquidityNode } from '../src';
 import { TickEncoder } from '../src';
 
 const TEST_NODES: LiquidityNode[] = [
+  { tick: TickEncoder.encode({ limit: 0n, duration: 0, rate: 0 }), available: 0n },
   { tick: TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }), available: 500n },
-  { tick: TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }), available: 250n },
-  { tick: TickEncoder.encode({ limit: 50n, duration: 2, rate: 1 }), available: 250n },
-  { tick: TickEncoder.encode({ limit: 50n, duration: 2, rate: 2 }), available: 500n },
-  { tick: TickEncoder.encode({ limit: 100n, duration: 1, rate: 1 }), available: 300n },
-  { tick: TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }), available: 200n },
-  { tick: TickEncoder.encode({ limit: 125n, duration: 1, rate: 0 }), available: 100n },
-  { tick: TickEncoder.encode({ limit: 125n, duration: 1, rate: 1 }), available: 200n },
-  { tick: TickEncoder.encode({ limit: 150n, duration: 0, rate: 0 }), available: 50n },
-  { tick: TickEncoder.encode({ limit: 150n, duration: 0, rate: 1 }), available: 75n },
+  { tick: TickEncoder.encode({ limit: 50n, duration: 0, rate: 2 }), available: 0n },
+  { tick: TickEncoder.encode({ limit: 50n, duration: 1, rate: 0 }), available: 250n },
+  { tick: TickEncoder.encode({ limit: 50n, duration: 2, rate: 1 }), available: 200n },
+  { tick: TickEncoder.encode({ limit: 100n, duration: 0, rate: 1 }), available: 150n },
+  { tick: TickEncoder.encode({ limit: 100n, duration: 1, rate: 2 }), available: 100n },
 ];
 
 describe('TickRouter', function () {
@@ -21,83 +18,344 @@ describe('TickRouter', function () {
     router = new TickRouter([7 * 86400, 14 * 86400, 30 * 86400], [3170979198n, 9512937595n, 15854895991n]);
   });
 
-  it('#_mapDuration', function () {
-    expect(router._mapDuration(3 * 86400)).toEqual(0);
-    expect(router._mapDuration(7 * 86400)).toEqual(0);
-    expect(router._mapDuration(10 * 86400)).toEqual(1);
-    expect(router._mapDuration(14 * 86400)).toEqual(1);
-    expect(router._mapDuration(25 * 86400)).toEqual(2);
-    expect(router._mapDuration(30 * 86400)).toEqual(2);
-    expect(() => router._mapDuration(35 * 86400)).toThrow();
+  it('#_decodeNodes', function () {
+    expect(router._decodeNodes(TEST_NODES)).toEqual([
+      { tick: { limit: 0n, duration: 0, rate: 0, reserved: 0n }, available: 0n },
+      { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+      { tick: { limit: 50n, duration: 0, rate: 2, reserved: 0n }, available: 0n },
+      { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+      { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      { tick: { limit: 100n, duration: 0, rate: 1, reserved: 0n }, available: 150n },
+      { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+    ]);
   });
 
   it('#_filterNodes', function () {
-    expect(router._filterNodes(TEST_NODES, 3 * 86400).map((n) => n.tick)).toEqual([
-      { limit: 25n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 50n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 100n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 125n, duration: 1, rate: 0, reserved: 0n },
-      { limit: 150n, duration: 0, rate: 0, reserved: 0n },
+    const nodes = router._decodeNodes(TEST_NODES);
+
+    /* 3 days */
+    expect(router._filterNodes(nodes, 3 * 86400)).toEqual([
+      { tick: { limit: 0n, duration: 0, rate: 0, reserved: 0n }, available: 0n },
+      { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+      { tick: { limit: 50n, duration: 0, rate: 2, reserved: 0n }, available: 0n },
+      { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+      { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      { tick: { limit: 100n, duration: 0, rate: 1, reserved: 0n }, available: 150n },
+      { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
     ]);
 
-    expect(router._filterNodes(TEST_NODES, 10 * 86400).map((n) => n.tick)).toEqual([
-      { limit: 25n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 50n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 100n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 125n, duration: 1, rate: 0, reserved: 0n },
+    /* 10 days */
+    expect(router._filterNodes(nodes, 10 * 86400)).toEqual([
+      { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+      { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+      { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
     ]);
 
-    expect(router._filterNodes(TEST_NODES, 30 * 86400).map((n) => n.tick)).toEqual([
-      { limit: 25n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 50n, duration: 2, rate: 0, reserved: 0n },
-      { limit: 100n, duration: 2, rate: 0, reserved: 0n },
+    /* 30 days */
+    expect(router._filterNodes(nodes, 30 * 86400)).toEqual([
+      { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+      { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
     ]);
+
+    /* 35 days */
+    expect(router._filterNodes(nodes, 35 * 86400)).toEqual([]);
+  });
+
+  it('#_aggregateNodes', function () {
+    const nodes = router._decodeNodes(TEST_NODES);
+
+    /* 3 days */
+    expect(router._aggregateNodes(router._filterNodes(nodes, 3 * 86400))).toEqual([
+      [{ tick: { limit: 0n, duration: 0, rate: 0, reserved: 0n }, available: 0n }],
+      [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+      [
+        { tick: { limit: 50n, duration: 0, rate: 2, reserved: 0n }, available: 0n },
+        { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+        { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      ],
+      [
+        { tick: { limit: 100n, duration: 0, rate: 1, reserved: 0n }, available: 150n },
+        { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+      ],
+    ]);
+
+    /* 10 days */
+    expect(router._aggregateNodes(router._filterNodes(nodes, 10 * 86400))).toEqual([
+      [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+      [
+        { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+        { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      ],
+      [{ tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n }],
+    ]);
+
+    /* 30 days */
+    expect(router._aggregateNodes(router._filterNodes(nodes, 30 * 86400))).toEqual([
+      [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+      [{ tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n }],
+    ]);
+
+    /* 35 days */
+    expect(router._aggregateNodes(router._filterNodes(nodes, 35 * 86400))).toEqual([]);
+  });
+
+  it('#_traverseNodes', function () {
+    const nodes = router._decodeNodes(TEST_NODES);
+
+    /* 10 days */
+    expect(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 10 * 86400)), 1)).toEqual([
+      {
+        nodes: [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+        amount: 25n,
+        cost: 25n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+        ],
+        amount: 50n,
+        cost: 50n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+          { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+        ],
+        amount: 100n,
+        cost: 200n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+        ],
+        amount: 50n,
+        cost: 75n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+          { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+        ],
+        amount: 100n,
+        cost: 225n,
+      },
+    ]);
+    expect(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 10 * 86400)), 3)).toEqual([
+      {
+        nodes: [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+        amount: 75n,
+        cost: 75n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+        ],
+        amount: 150n,
+        cost: 150n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+          { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+        ],
+        amount: 250n,
+        cost: 450n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+        ],
+        amount: 150n,
+        cost: 225n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+          { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+        ],
+        amount: 250n,
+        cost: 525n,
+      },
+    ]);
+
+    /* 30 days */
+    expect(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 30 * 86400)), 1)).toEqual([
+      {
+        nodes: [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+        amount: 25n,
+        cost: 25n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+        ],
+        amount: 50n,
+        cost: 75n,
+      },
+    ]);
+    expect(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 30 * 86400)), 3)).toEqual([
+      {
+        nodes: [{ tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n }],
+        amount: 75n,
+        cost: 75n,
+      },
+      {
+        nodes: [
+          { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+          { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+        ],
+        amount: 150n,
+        cost: 225n,
+      },
+    ]);
+
+    /* 35 days */
+    expect(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 35 * 86400)), 1)).toEqual([]);
+    expect(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 35 * 86400)), 3)).toEqual([]);
+  });
+
+  it('#_findBestRoute', function () {
+    const nodes = router._decodeNodes(TEST_NODES);
+
+    /* 10 days */
+    expect(
+      router._findBestRoute(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 10 * 86400)), 1)),
+    ).toEqual({
+      nodes: [
+        { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+        { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+        { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+      ],
+      amount: 100n,
+      cost: 200n,
+    });
+    expect(
+      router._findBestRoute(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 10 * 86400)), 3)),
+    ).toEqual({
+      nodes: [
+        { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+        { tick: { limit: 50n, duration: 1, rate: 0, reserved: 0n }, available: 250n },
+        { tick: { limit: 100n, duration: 1, rate: 2, reserved: 0n }, available: 100n },
+      ],
+      amount: 250n,
+      cost: 450n,
+    });
+
+    /* 30 days */
+    expect(
+      router._findBestRoute(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 30 * 86400)), 1)),
+    ).toEqual({
+      nodes: [
+        { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+        { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      ],
+      amount: 50n,
+      cost: 75n,
+    });
+    expect(
+      router._findBestRoute(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 30 * 86400)), 3)),
+    ).toEqual({
+      nodes: [
+        { tick: { limit: 25n, duration: 2, rate: 0, reserved: 0n }, available: 500n },
+        { tick: { limit: 50n, duration: 2, rate: 1, reserved: 0n }, available: 200n },
+      ],
+      amount: 150n,
+      cost: 225n,
+    });
+
+    /* 35 days */
+    expect(
+      router._findBestRoute(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 35 * 86400)), 1)),
+    ).toEqual({
+      nodes: [],
+      amount: 0n,
+      cost: 0n,
+    });
+    expect(
+      router._findBestRoute(router._traverseNodes(router._aggregateNodes(router._filterNodes(nodes, 35 * 86400)), 3)),
+    ).toEqual({
+      nodes: [],
+      amount: 0n,
+      cost: 0n,
+    });
   });
 
   it('#forecast', function () {
-    expect(router.forecast(TEST_NODES, 3 * 86400, 1)).toEqual(150n);
-    expect(router.forecast(TEST_NODES, 3 * 86400, 3)).toEqual(425n);
-    expect(router.forecast(TEST_NODES, 10 * 86400, 1)).toEqual(125n);
-    expect(router.forecast(TEST_NODES, 10 * 86400, 3)).toEqual(375n);
-    expect(router.forecast(TEST_NODES, 30 * 86400, 1)).toEqual(100n);
-    expect(router.forecast(TEST_NODES, 30 * 86400, 3)).toEqual(300n);
+    /* 3 days */
+    expect(router.forecast(TEST_NODES, 3 * 86400, 1)).toEqual(100n);
+    expect(router.forecast(TEST_NODES, 3 * 86400, 3)).toEqual(300n);
+
+    /* 10 days */
+    expect(router.forecast(TEST_NODES, 10 * 86400, 1)).toEqual(100n);
+    expect(router.forecast(TEST_NODES, 10 * 86400, 3)).toEqual(250n);
+
+    /* 30 days */
+    expect(router.forecast(TEST_NODES, 30 * 86400, 1)).toEqual(50n);
+    expect(router.forecast(TEST_NODES, 30 * 86400, 3)).toEqual(150n);
+
+    /* 35 days */
+    expect(router.forecast(TEST_NODES, 35 * 86400, 1)).toEqual(0n);
+    expect(router.forecast(TEST_NODES, 35 * 86400, 3)).toEqual(0n);
+
+    /* 10 days with reduced number of nodes */
+    expect(router.forecast(TEST_NODES, 10 * 86400, 3, 1)).toEqual(100n);
   });
 
   it('#route', function () {
-    expect(router.route(TEST_NODES, 125n, 3 * 86400, 1, 4)).toEqual([
-      TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 125n, duration: 1, rate: 0 }),
-      TickEncoder.encode({ limit: 150n, duration: 0, rate: 0 }),
-    ]);
-    expect(router.route(TEST_NODES, 425n, 3 * 86400, 3, 4)).toEqual([
-      TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 125n, duration: 1, rate: 0 }),
-      TickEncoder.encode({ limit: 150n, duration: 0, rate: 0 }),
-    ]);
-    expect(router.route(TEST_NODES, 125n, 10 * 86400, 1, 4)).toEqual([
+    /* 3 days */
+    expect(router.route(TEST_NODES, 100n, 3 * 86400, 1)).toEqual([
       TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 125n, duration: 1, rate: 0 }),
+      TickEncoder.encode({ limit: 50n, duration: 1, rate: 0 }),
+      TickEncoder.encode({ limit: 100n, duration: 0, rate: 1 }),
     ]);
-    expect(router.route(TEST_NODES, 375n, 10 * 86400, 3, 4)).toEqual([
+    expect(router.route(TEST_NODES, 300n, 3 * 86400, 3)).toEqual([
       TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 125n, duration: 1, rate: 0 }),
+      TickEncoder.encode({ limit: 50n, duration: 1, rate: 0 }),
+      TickEncoder.encode({ limit: 100n, duration: 0, rate: 1 }),
     ]);
-    expect(router.route(TEST_NODES, 100n, 30 * 86400, 1, 4)).toEqual([
+
+    /* 10 days */
+    expect(router.route(TEST_NODES, 100n, 10 * 86400, 1)).toEqual([
       TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }),
+      TickEncoder.encode({ limit: 50n, duration: 1, rate: 0 }),
+      TickEncoder.encode({ limit: 100n, duration: 1, rate: 2 }),
     ]);
-    expect(router.route(TEST_NODES, 300n, 30 * 86400, 3, 4)).toEqual([
+    expect(router.route(TEST_NODES, 250n, 10 * 86400, 3)).toEqual([
       TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 50n, duration: 2, rate: 0 }),
-      TickEncoder.encode({ limit: 100n, duration: 2, rate: 0 }),
+      TickEncoder.encode({ limit: 50n, duration: 1, rate: 0 }),
+      TickEncoder.encode({ limit: 100n, duration: 1, rate: 2 }),
     ]);
-    expect(() => router.route(TEST_NODES, 500n, 30 * 86400, 5, 4)).toThrow();
+
+    /* 30 days */
+    expect(router.route(TEST_NODES, 50n, 30 * 86400, 1)).toEqual([
+      TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }),
+      TickEncoder.encode({ limit: 50n, duration: 2, rate: 1 }),
+    ]);
+    expect(router.route(TEST_NODES, 100n, 30 * 86400, 3)).toEqual([
+      TickEncoder.encode({ limit: 25n, duration: 2, rate: 0 }),
+      TickEncoder.encode({ limit: 50n, duration: 2, rate: 1 }),
+    ]);
+
+    /* 30 days over available */
+    expect(() => router.route(TEST_NODES, 51n, 30 * 86400, 1)).toThrow();
+
+    /* 10 days with reduced number of nodes */
+    expect(router.route(TEST_NODES, 100n, 10 * 86400, 3, 1)).toEqual([
+      TickEncoder.encode({ limit: 100n, duration: 1, rate: 2 }),
+    ]);
+
+    /* 35 days */
+    expect(router.route(TEST_NODES, 0n, 35 * 86400, 1)).toEqual([]);
+    expect(() => router.route(TEST_NODES, 1n, 35 * 86400, 1)).toThrow();
   });
 });
